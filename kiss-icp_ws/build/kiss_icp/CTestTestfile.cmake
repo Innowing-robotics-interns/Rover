16eb897d32f2f3d6e0d6e54e43af95f2d15ae30d
iwintern <intern.innowing@gmail.com>
2024-07-16 17:10:49 +0800

    add kiss-icp_ws

---
+++ kiss-icp_ws/build/kiss_icp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/nova_ssd/workspaces/kiss-icp_ws/src/kiss-icp/ros
+# Build directory: /mnt/nova_ssd/workspaces/kiss-icp_ws/build/kiss_icp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("kiss_icp")
