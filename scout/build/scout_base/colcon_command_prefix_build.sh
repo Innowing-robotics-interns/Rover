b8558e203b09d14c31accb47dc62103f412e3248
iwintern <intern.innowing@gmail.com>
2024-07-22 11:48:43 +0800

    July22 backup scout

---
+++ scout/build/scout_base/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/mnt/nova_ssd/workspaces/scout/install/scout_msgs/share/scout_msgs/package.sh"
+. "/mnt/nova_ssd/workspaces/scout/install/ugv_sdk/share/ugv_sdk/package.sh"
